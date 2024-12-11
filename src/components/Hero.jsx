3c347e205ed5f3990e47539d340af9282b3679f9
arisarisyi <alarisyi@gmail.com>
2024-12-11 11:12:35 +0800

    create lazy loading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,6 +36,7 @@ const Hero = () => {
               initial={{ x: 100, opacity: 0 }}
               animate={{ x: 0, opacity: 1 }}
               transition={{ duration: 1, delay: 1.5 }}
+              loading="lazy"
             />
           </div>
         </div>
